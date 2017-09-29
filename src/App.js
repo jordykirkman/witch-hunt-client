a952f88372bae017fa667ebc015a81e1ed3687cf
Jordy Kirkman <jordyakirkman@gmail.com>
2017-09-29 15:27:35 -0600

    better instructions

--- src/App.js
+++ src/App.js
@@ -416,18 +416,34 @@ class App extends Component {
                 </div>
                 <div className="columns">
                   <div className='column is-third'>
-                    <img src={sunLogo} className="how-to-play-icon" alt="witch hunt day" />
                     <h4 className="title is-4">
-                      Day
-                    </h4>
-                    The village members identify the guilty who is then put on trial. After their defense, vote to spare them or put them to death.
-                  </div>
-                  <div className='column is-third'>
-                    <img src={moonLogo} className="how-to-play-icon" alt="witch hunt night" />
-                    <h4 className="title is-4">
+                      <img src={moonLogo} className="how-to-play-icon" alt="witch hunt night" />
                       Night
                     </h4>
-                    The hiding monster chooses a victim. 20% chance the victim survives.
+                    <ul className="instructions-list">
+                      <li>Villagers may visit one another to see if they are home or missing. 20% chance to fail.</li>
+                      <li>Monsters may choose a victim to "mark" for death. Monsters cannot mark eachother and are seen as missing.</li>
+                      <li>If a villager checks on someone else successfully, they are seen as missing.</li>
+                    </ul>
+                  </div>
+                  <div className='column is-third'>
+                    <h4 className="title is-4">
+                      <img src={sunLogo} className="how-to-play-icon" alt="witch hunt day" />
+                      Day
+                    </h4>
+                    <ul className="instructions-list">
+                      <li>The village may vote to put someone on trial (anonymously).</li>
+                      <li>When on trial, a villager may defend themselves, point blame. The others vote to spare or kill.</li>
+                    </ul>
+                  </div>
+                  <div className='column is-third'>
+                    <h4 className="title is-4">
+                      To Win
+                    </h4>
+                    <ul className="instructions-list">
+                      <li>Villagers must kill the hiding monsters.</li>
+                      <li>If living monsters are equal to unmarked/unkilled villagers, marked villagers die and monsters win.</li>
+                    </ul>
                   </div>
                 </div>
               </div>
